75d06a935fde4316500e4ead105d4b225b7a093f
whitneywhitehill15 <70212230+whitneywhitehill15@users.noreply.github.com>
2021-06-26 14:29:32 -0500

    Added honor code

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { useUser } from "../providers/UserContextProvider";
-import { Container, Row, Button, Col, Spinner } from "react-bootstrap"
+import { Container, Row, Button, Col, Spinner, Jumbotron } from "react-bootstrap"
 import { authProvider } from '../providers/LoginUser'
 
 export const LoginPage = () => {
   const [isLoading, setIsLoading] = React.useState(false);
 
-  const login = ()=>{
-      setIsLoading(true)
-      authProvider.login({})
+  const login = () => {
+    setIsLoading(true)
+    authProvider.login({})
       .then(setIsLoading(false))
       .catch(err => alert(err))
   }
@@ -18,13 +18,22 @@ export const LoginPage = () => {
       <Row>
         <Col>
           <Container>
+            <Jumbotron>
+              <h1>Honor Code Policy</h1>
+              <p>
+                This application was developed to support the Southwestern Community with cooperation from the Center for Academic Success. As such, the restrictions outlined in the Southwestern University Honor Code and the Student handbook apply to any actions you take within the application.
+                By logging in you are agreeing that you will not break university Honor Code or Student Handbook Policy.
+              </p>
+              <Button href="https://www.southwestern.edu/offices/success/">Click here for more info</Button>
+            </Jumbotron>
+
             {isLoading ? (
               <Spinner animation="border" role="status">
                 <span className="sr-only">Loading...</span>
               </Spinner>
             ) : (
-              <Button onClick={login}>Log in with Google</Button>
-            )}
+                <Button onClick={login}>Log in with Google</Button>
+              )}
           </Container>
         </Col>
       </Row>
