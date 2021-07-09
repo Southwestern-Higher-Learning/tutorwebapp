ac46fd364aa376825e391d9f7d68705f5084053b
whitneywhitehill15 <70212230+whitneywhitehill15@users.noreply.github.com>
2021-07-09 15:25:40 -0500

    Cleaned up code for merge

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,67 +1,56 @@
 import React from 'react';
 import { authProvider } from '../providers/LoginUser'
-import { useUser } from '../providers/UserContextProvider'
-import { Spinner, Container, Navbar, Nav, Jumbotron, Card } from 'react-bootstrap'
+import { Spinner, Container, Navbar, Nav, Jumbotron } from 'react-bootstrap'
 import { House, Person } from 'react-bootstrap-icons'
 
+
 export const HomePage = () => {
-    const { state, dispatch } = useUser()
     const [isLoading, setIsLoading] = React.useState(true)
 
     const flipLoading = () => {
         setIsLoading(!isLoading)
     }
 
-    console.log(state)
     React.useEffect(() => {
-
         const { searchParams } = new URL(window.location.href);
         const code = searchParams.get('code');
-
-
         if (code) {
-            console.log("homepage if code")
             authProvider.login({ code })
                 .then((res) => {
-                    dispatch({ message: "SET", payload: res })
-
                     flipLoading()
                 })
                 .catch(err => alert(err))
         }
-
     })
 
     return isLoading ? (<Spinner></Spinner>) : (
         <Container>
             <Navbar className="navBar">
                 <Nav className="navigationBar">
-                    <Nav.Link>  <House /> </Nav.Link>
-                    <Nav.Link>Find a Tutor</Nav.Link>
-                    <Nav.Link><Person /></Nav.Link>
+                    <Nav.Link href="./HomePage">  <House /> </Nav.Link>
+                    <Nav.Link href="./SearchTutor.js">Find a Tutor</Nav.Link>
+                    <Nav.Link ><Person /></Nav.Link>
                 </Nav>
             </Navbar>
-            <Container className="jumbotron">
-                <Jumbotron>
-                    <h1>Welcome top the SU tutor app!</h1>
-                    <p>
-                        Welcome to the SU tutor web page. This webiste will serve as a place holder to access, search for, and learn
-                        more information about the tutoring offered within the varying departments.
+            <Jumbotron>
+                <h1>Welcome top the SU tutor app!</h1>
+                <p>
+                    Welcome to the SU tutor web page. This webiste will serve as a place holder to access, search for, and learn
+                    more information about the tutoring offered within the varying departments.
                         </p>
-                </Jumbotron>
-            </Container>
-            <div className="wholediv">
+            </Jumbotron>
+            <div className="info">
                 <Container>
                     <div className="class">
-                        <div className="extra">
+                        <div className="additionalInfo">
                             <h5>Before your appointment</h5>
                         fill out this form
                     </div>
-                        <div className="extra">
+                        <div className="additionalInfo">
                             <h5>Additional Resources</h5>
                             <a href="https://www.southwestern.edu/offices/success/">Center for Academic Success</a>
                         </div>
-                        <div className="extra">
+                        <div className="additionalInfo">
                             <h5>Tips and Tricks</h5>
                     Go to tutoring the day before the assignment is due.
                     </div>
