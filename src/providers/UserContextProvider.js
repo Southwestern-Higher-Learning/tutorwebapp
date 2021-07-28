7171f3964dcd2648bd90e9bcdda18607987956d5
whitneywhitehill15 <70212230+whitneywhitehill15@users.noreply.github.com>
2021-07-27 21:49:31 -0500

    Trying stuff

--- src/providers/UserContextProvider.js
+++ src/providers/UserContextProvider.js
@@ -1,40 +1,10 @@
 import React from 'react';
-import {authProvider} from './LoginUser'
-
-let initialState = {
-    user: null,
-    access_token: null
-}
-
+import { authProvider } from './LoginUser'
 
 const UserContext = React.createContext()
 
-const reducer = (state, action) => {
-    switch(action.message){
-        case "SET_USER":
-            return {
-                ...state,
-                user: action.payload
-            }
-        case "SET_TOKEN":
-            return {
-                ...state,
-                access_token: action.payload
-            }
-        case "SET":
-            localStorage.setItem("accessKey", action.payload.access_token);
-            return {
-                user: action.payload.user,
-                access_token: action.payload.access_token
-            }
-        default:
-            return state;
-    }
-}
-
-export default function ProviderComponent({children}){
-    const [state, dispatch] = React.useReducer(reducer, initialState)
-    const contextValue = React.useMemo(() => { return {state, dispatch}}, [state, dispatch])
+export default function ProviderComponent({ children }) {
+    const [state, dispatch] = React.useState({})
 
     const refreshUser = authProvider.RefreshUser
 
@@ -46,6 +16,6 @@ export default function ProviderComponent({children}){
     return <UserContext.Provider value={providerValue}>{children}</UserContext.Provider>
 }
 
-export function useUser(){
+export function useUser() {
     return React.useContext(UserContext)
 }
