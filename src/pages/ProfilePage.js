1bcb9bf9df6d23a92424f3ce16551521d8c286fc
whitneywhitehill15 <70212230+whitneywhitehill15@users.noreply.github.com>
2021-08-05 22:00:22 -0500

    Started working on profile screen

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,15 +1,36 @@
 import React, { useState } from 'react';
+import { Row, Form } from 'react-bootstrap';
 import { NavigationBar } from '../components/NavigatonBar';
 
 export const ProfilePage = () => {
 
-    const [state, setstate] = useState(JSON.parse(localStorage.getItem("user")))
-    console.log(`current state: ${state}`)
+    const [state] = useState(JSON.parse(localStorage.getItem("user")))
+    // console.log(`current state: ${state}`)
 
     return (
         <div>
             <NavigationBar></NavigationBar>
-            <img src={state?.profile_url} alt="profile" />
+            <div className="profileContainer">
+                <Row>
+                    <div className="welcomeText">
+                        Welcome to your profile page
+                    </div>
+                </Row>
+                <Row>
+                    <img className="profileImage" src={state?.profile_url} alt="profile" />
+                    <div className="nameText">{state?.first_name} {state?.last_name}</div>
+                </Row>
+                <Row>
+                    <div className="form">
+                        <Form.Control
+                            as="textarea"
+                            placeholder="Write your bio here"
+                            style={{ height: '100px' }}
+                        />
+                    </div>
+                </Row>
+
+            </div>
         </div>
     )
 }
