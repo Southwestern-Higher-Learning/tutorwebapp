91b9366e743c32786c2fcae26b1af8bd77f5d84c
DeclanWJ <declanj2000@hotmail.com>
2021-08-18 14:16:54 -0500

    Changes to profile
    Reformatted , cards instead of rows. Changed about me section. Added some depth. NEEDS: Way to show tutor/student. Image in background, personal/choose.

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react';
 import React, { useState } from 'react';
 import { Row, Form, Button, Modal, Card } from 'react-bootstrap';
 import { NavigationBar } from '../components/NavigatonBar';
@@ -7,16 +6,27 @@ import { UpdateDescription } from '../providers/UpdateAboutMe';
 export const ProfilePage = () => {
 
     const [state] = useState(JSON.parse(localStorage.getItem("user")))
-    let [textValue, setTextValue] = useState(state?.description)
+    let [textValue, setTextValue] = useState(state.description)
 
 
     const onFormSubmit = async e => {
         e.preventDefault()
         const formData = new FormData(e.target),
             userInput = Object.fromEntries(formData.entries())
+        console.log(`form data object: ${userInput.description}`)
+        console.log(`state stuff: ${state.description}`)
         const response = await UpdateDescription(userInput.description, localStorage.getItem('access_token'))
+        console.log(response)
         if (response) {
-            setTextValue(userInput?.description);
+            try {
+                setTextValue = () => {
+                    textValue(userInput.description)
+                }
+                this.forceUpdate()
+            } catch (error) {
+                // add better error handling
+                console.error()
+            }
         }
     }
     const [isOpen, setIsOpen] = React.useState(false);
@@ -32,27 +42,40 @@ export const ProfilePage = () => {
         <div className="profilePage">
             <NavigationBar></NavigationBar>
             <div className="profileContainer">
+
                 <Row className="welcomeRow">
                     <div className="welcomeText">
                         Welcome to your profile page
                     </div>
+                </Row>
+
+                <Card className="userCard">
+
+                    <img className="profileImage" src={state?.profile_url} alt="profile" />
+                
+                    <div className="nameText">{state?.first_name} {state?.last_name}</div>
+                
+                <Row className="aboutMeRow">
                     <div className="aboutMeDetails">
-                        <Card>
+                        <Card className = "aboutCard">
+                            <Card.Title>About</Card.Title>
                             <Card.Body>
                                 Your profile is a place to share a little about yourself. If you are a tutor
                                 share what you tutor and potential approaches you take when tutoring. If
                                 you are a student feel free to share ways that help you learn your major, minor, etc.
                             </Card.Body>
+                            <Button varient="primary" className="aboutMeButton" onClick={showModal}> Edit </Button>
                         </Card>
                     </div>
+
+
                 </Row>
-                <Row className="imageRow">
-                    <img className="profileImage" src={state?.profile_url} alt="profile" />
-                </Row>
-                <Row className="aboutRow">
-                    <div className="nameText">{state?.first_name} {state?.last_name}</div>
-                </Row>
-                <button className="aboutMeButton" onClick={showModal}> Edit your About me</button>
+
+
+                </Card>
+
+                
+        
                 <Modal show={isOpen} onHide={hideModal}>
                     <Modal.Header>
                         <Modal.Title>Update Your About Me</Modal.Title>
@@ -65,16 +88,16 @@ export const ProfilePage = () => {
                                     name="description"
                                     as="textarea"
                                     placeholder="Update your about me here"
-                                    style={{ height: '100px', width: '600px' }}
                                 />
                                 <Button className='submit-button' variant="primary" type="submit" onClick={hideModal}>Submit</Button>
                             </Form>
                         </div>
                     </Modal.Body>
+                    <Modal.Footer>
+
+                    </Modal.Footer>
                 </Modal>
-            </div>
-            <div className="bio">
-                {state?.description}
+
             </div>
         </div>
     )
