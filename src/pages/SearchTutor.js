06ec262c97c531b1f61e38660168101915fbafd3
whitneywhitehill15 <70212230+whitneywhitehill15@users.noreply.github.com>
2021-08-17 21:58:54 -0500

    Added ability to search for tutor

--- src/pages/SearchTutor.js
+++ src/pages/SearchTutor.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Card, Form, Button } from 'react-bootstrap'
 import { NavigationBar } from '../components/NavigatonBar'
+import { TutorQuery } from '../providers/SearchTutor';
+
 
 
 
 export const SearchTutor = () => {
+    const [state] = useState(JSON.parse(localStorage.getItem("user")))
+
+    const onFormSubmit = async e => {
+        e.preventDefault()
+        const formData = new FormData(e.target),
+            userInput = Object.fromEntries(formData.entries())
+        console.log(userInput.tutor)
+        TutorQuery({ isName: true, param: `${userInput}`, token: `${localStorage.getItem("access_token")}` })
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => console.log(err));
+
+    }
+
+
+
     return (
         <Container>
             <NavigationBar></NavigationBar>
             <Card>
                 <Card.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form onSubmit={onFormSubmit}>
+                        <Form.Group>
                             <Form.Label>Search for a Tutor</Form.Label>
-                            <Form.Control type="email" placeholder='Search for a tutor by name or subject' />
+                            <Form.Control type="text"
+                                name="tutor"
+                                placeholder="Search for a tutor"
+                                style={{ height: '100px', width: '600px' }} />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
+                        <Button variant="primary" type="submit" >
                             Search
                         </Button>
                     </Form>
--- src/providers/SearchTutor.js
+++ src/providers/SearchTutor.js
@@ -1,11 +1,21 @@
 import axios from 'axios'
 
-export const SearchTutor = (searchParam) => {
+export const TutorQuery = (searchParam) => {
+    // {
+    // isName: true ? false,
+    // param: category ? name
+    // }
     if (searchParam) {
         if (searchParam.isName) {
             // searching by name
             // return axios.get
-            return axios.get(`https://tutor.jakegut.com/user/?is_tutor=true&first_name__icontains=${searchParam.param}&_sort=first_name&_order=asc&_start=0&_end=20`)
+            return axios.get(`https://tutor.jakegut.com/user/?is_tutor=true&first_name__icontains=${searchParam.param}&_sort=first_name&_order=asc&_start=0&_end=20`,
+                {
+                    headers: {
+                        'content-type': 'application/json',
+                        'authorization': `Bearer ${searchParam?.token}`
+                    }
+                })
                 .then((response) => { return response.data })
                 .catch(error => { return Promise.reject(error) })
         } else {
